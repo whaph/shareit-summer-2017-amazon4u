f2158597463348c0c7a47f34d5e87d5c6b97003b
0SM0S <jkeppeler@hm.edu>
2017-04-19 17:40:19 +0200

    grobe struktur

---
+++ src/main/java/edu/hm/server/server.java
@@ -0,0 +1,48 @@
+package edu.hm.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by jupiter on 4/19/17.
+ */
+public class server {
+    public static final int PORT = 8082;
+
+
+    public void listen(int port) throws IOException {
+        ServerSocket servSock = new ServerSocket(port);
+
+        while(true){
+            Socket sock = servSock.accept();
+            BufferedReader fromClient = new BufferedReader(
+                    new InputStreamReader(sock.getInputStream()));
+            String request = "";
+
+            for (String line = fromClient.readLine();
+                 line != null && line.length() > 0;
+                 line = fromClient.readLine()){
+                request += line;
+            }
+
+            //TODO parse request
+
+            String response = ""; // add response
+
+            BufferedWriter toClient = new BufferedWriter(
+                    new OutputStreamWriter(sock.getOutputStream()));
+
+
+            if(servSock.isBound()) {
+                toClient.write("HTTP/1.1 200 Ok\r\n");
+                toClient.write("Content-length: " + response.length() + "\r\n");
+                toClient.write(response);
+                toClient.write("\r\n");
+                toClient.flush();
+
+            }
+        }
+
+    }
+}
