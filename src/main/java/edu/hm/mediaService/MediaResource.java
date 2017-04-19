88b9e0f9448e12122e1e7c1b87fb8fd4f576a6a6
0SM0S <jkeppeler@hm.edu>
2017-04-19 16:56:13 +0200

    mediaResource

--- src/main/java/edu/hm/mediaService/MediaResource.java
+++ src/main/java/edu/hm/mediaService/MediaResource.java
@@ -13,19 +13,47 @@ public class MediaResource {
     private final MediaService mediaService = new MediaServiceImplementation();
 
 
-    public Response createMedium(Medium medium){
 
-        MediaServiceResult msr;
-        if(medium instanceof Book){
-            msr = mediaService.addBook((Book)medium);
-        }else if(medium instanceof Disc){
-            msr = mediaService.addDisc((Disc)medium);
-        }else{
-            throw new IllegalArgumentException("Not a medium");
-        }
+
+
+    public Response createBook(Book book){
+        MediaServiceResult msr = getMediaService().addBook(book);
         return new Response(msr);
     }
 
+    public Response createDisc(Disc disc){
+        MediaServiceResult msr = getMediaService().addDisc(disc);
+        return new Response(msr);
+    }
+
+    private MediaService getMediaService(){
+        return this.mediaService;
+    }
+
+    public Response getBooks(){
+        Medium[] books = getMediaService().getBooks();
+        //TODO
+        return new Response(MediaServiceResult.valueOf(""));
+    }
+
+
+    public Response getDiscs(){
+        Medium[] books = getMediaService().getDiscs();
+        //TODO
+        return new Response(MediaServiceResult.valueOf(""));
+    }
+
+    public Response updateBook(Book book){
+        MediaServiceResult msr = getMediaService().updateBook(book);
+        return new Response(msr);
+
+    }
+
+    public Response updateDisc(Disc disc){
+        MediaServiceResult msr = getMediaService().updateDisc(disc);
+        return new Response(msr);
+
+    }
 
 
     private class Response {
