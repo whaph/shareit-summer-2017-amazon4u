0b9d610114fb9254f5373099237cb71e2f525dbe
0SM0S <jkeppeler@hm.edu>
2017-04-19 18:34:20 +0200

    grobe struktur

--- src/main/java/edu/hm/JettyStarter.java
+++ src/main/java/edu/hm/JettyStarter.java
@@ -1,5 +1,5 @@
 package edu.hm;
-
+import edu.hm.server.*;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.webapp.*;
@@ -23,10 +23,12 @@ public class JettyStarter {
      */
     public static void main(String... args) throws Exception {
         Server jetty = new Server(PORT);
-
+        //Amazon4UServer server = new Amazon4UServer();
+        //server.listen(8082);
         jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
         jetty.start();
         System.out.println("Jetty listening on port " + PORT);
         jetty.join();
+
     }
 }
