66be38a712d72ab222014e0a5aeb605feac85d02
whaph <whaph@outlook.com>
2017-04-19 16:31:29 +0200

    + MediaServiceResult.java
    enum still missing enum values

--- src/main/java/edu/hm/mediaService/MediaServiceResult.java
+++ src/main/java/edu/hm/mediaService/MediaServiceResult.java
@@ -1,10 +1,40 @@
 package edu.hm.mediaService;
 
 /**
- * Created by jupiter on 4/19/17.
+ * Representation of a MediaServiceResult
+ * Containing information: Status and Code
  */
 public enum MediaServiceResult {
 
+    /*
+    *  TODO - Create some values
+    */
+    ;
 
+    private final int code;
+    private final Status status;
 
+    private class Status {
+        private final String status;
+        Status(String status){
+            this.status = status;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+    }
+
+    MediaServiceResult(int code, Status status){
+        this.code = code;
+        this.status = status;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
 }
